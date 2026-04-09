9c3bf4b9b340fd88d8e8af522714f9e9c1b880d9
Christian Saloň <chsalon02@gmail.com>
2026-04-10 00:57:33 +0200

    fix: Download only necessary div2k2018 datasets

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -13,18 +13,6 @@ cd train
 mkdir -p div2k_2018
 cd div2k_2018
 
-wget -c http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_x8.zip
-unzip -n -j DIV2K_train_LR_x8.zip -d lr_x8
-
-wget -c http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_mild.zip
-unzip -n -j DIV2K_train_LR_mild.zip -d lr_mild
-
-wget -c http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_difficult.zip
-unzip -n -j DIV2K_train_LR_difficult.zip -d lr_difficult
-
-wget -c http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_wild.zip
-unzip -n -j DIV2K_train_LR_wild.zip -d lr_wild
-
 wget -c http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip
 unzip -n -j DIV2K_train_HR.zip -d hr
 
@@ -43,10 +31,10 @@ mkdir -p lr_lan_8x
 
 python ../../../utils/init_dataset.py --input hr --output .
 
-cd ../../
+cd ../../test
 
 # div2k 2018 validation dataset
-cd test
+mkdir -p div2k_2018
 
 wget -c http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip
 unzip -n -j DIV2K_valid_HR.zip -d hr
